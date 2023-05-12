2b8f771fa6bc753c8a47b899966874619b6caeee
Plasmon222 <1726170633@qq.com>
2023-05-12 22:27:14 +0800

    Site updated: 2023-05-12 22:27:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/01/01/Math/","2023/05/09/hello-world/","2023/05/10/SSM框架整合/","2023/05/10/二分模板/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/01/01/Math/","2023/05/10/SSM框架整合/","2023/05/10/二分模板/","2023/05/09/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
