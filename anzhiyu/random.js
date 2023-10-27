8757c76f5392c8b76f39d1ae279048989ca9b4c9
Plasm <1@2>
2023-10-27 10:41:06 +0800

    Site updated: 2023-10-27 10:41:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/18/2023年Unturned 服务器搭建与运维指南--Ubuntu/","2023/10/14/参观中山陵及近期事有感/","2023/10/10/Mybatis中常见错误/","2023/06/01/anzhiyu主题标签/","2023/05/10/建站心得/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/18/2023年Unturned 服务器搭建与运维指南--Ubuntu/","2023/10/10/Mybatis中常见错误/","2023/06/01/anzhiyu主题标签/","2023/10/14/参观中山陵及近期事有感/","2023/05/10/建站心得/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
